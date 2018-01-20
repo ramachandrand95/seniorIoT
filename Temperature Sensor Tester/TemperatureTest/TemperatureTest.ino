e978d7f255f6fba0aad6e37ddc9bb56bef45c855
Ramachandran <ramachandrand@ad.msoe.edu>
2018-01-20 16:55:22 -0600

    Both Temperature sensors are easily tested separatly

--- Temperature Sensor Tester/TemperatureTest/TemperatureTest.ino
+++ Temperature Sensor Tester/TemperatureTest/TemperatureTest.ino
@@ -9,11 +9,12 @@
 /********************************************************************/
 // Setup a oneWire instance to communicate with any OneWire devices  
 // (not just Maxim/Dallas temperature ICs) 
-OneWire oneWire(TEMP_ONE); 
-OneWire oneWire1(TEMP_TWO);
+OneWire oneWire1(TEMP_ONE); 
+OneWire oneWire2(TEMP_TWO);
 /********************************************************************/
 // Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
+DallasTemperature sensor1(&oneWire1);
+DallasTemperature sensor2(&oneWire2);
 /********************************************************************/ 
 void setup(void) 
 { 
@@ -21,17 +22,21 @@ void setup(void)
  Serial.begin(9600); 
  Serial.println("Dallas Temperature IC Control Library Demo"); 
  // Start up the library 
- sensors.begin(); 
+ sensor1.begin(); 
+ sensor2.begin();
 } 
 void loop(void) 
 { 
  // call sensors.requestTemperatures() to issue a global temperature 
  // request to all devices on the bus 
 /********************************************************************/
- sensors.requestTemperatures(); // Send the command to get temperature readings 
+ sensor1.requestTemperatures(); // Send the command to get temperature readings 
+ sensor2.requestTemperatures();
 /********************************************************************/
- Serial.print("Temperature is: "); 
- Serial.println(sensors.toFahrenheit(sensors.getTempCByIndex(0))); // Why "byIndex"?  
+ Serial.print("Shallow Temperature is: "); 
+ Serial.println(sensor1.toFahrenheit(sensor1.getTempCByIndex(0))); // Why "byIndex"?  
+ Serial.print("Deep Temperature is: ");
+ Serial.println(sensor2.toFahrenheit(sensor2.getTempCByIndex(0)));
    // You can have more than one DS18B20 on the same bus.  
    // 0 refers to the first IC on the wire 
    delay(1000); 
